8549590e3265c4831d69e724a2136233f9a9a867
macarthur <arthurbarre.js@gmail.com>
2019-12-19 10:14:35 +0100

    finish darkmode

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,12 +1,25 @@
-// const darkmodeBtn = document.querySelector('#darkmode');
+const darkmodeBtn = document.querySelector('#darkmode');
+const divs = document.querySelectorAll('#thumbnail')
+const link =  document.querySelector('#link-thumbnail');
 
-// const body =  document.querySelector('#body');
-// darkmodeBtn.addEventListener('click',()=>{
-//   // if(body.className !== 'toggle-isClicked'){
-//   //   body.className = 'toggle-isClicked'
-//   // } else {
-//   //   body.className = ''
-//   // }
 
-// })
-// console.log(darkmodeBtn)
+
+console.log(divs)
+const body =  document.querySelector('#body');
+darkmodeBtn.addEventListener('click', () => {
+  if(body.className !== 'toggle-isClicked'){
+    body.className = 'toggle-isClicked';
+  divs.forEach(div => {
+    div.className = 'toggle-isClicked'
+  });
+  link.style.color = 'white';
+  } else {
+    body.className = '';
+  divs.forEach(div => {
+    div.className = ''
+  });
+  link.style.color = 'black';
+  }
+
+})
+console.log(darkmodeBtn)
