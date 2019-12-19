c6d7539546b1f22034e4a3eb4cba2c3417fb4cc5
macarthur <arthurbarre.js@gmail.com>
2019-12-19 10:26:14 +0100

    add dm on article

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,25 +1,34 @@
 const darkmodeBtn = document.querySelector('#darkmode');
-const divs = document.querySelectorAll('#thumbnail')
+const divs = document.querySelectorAll('#thumbnail');
 const link =  document.querySelector('#link-thumbnail');
-
-
-
-console.log(divs)
 const body =  document.querySelector('#body');
-darkmodeBtn.addEventListener('click', () => {
-  if(body.className !== 'toggle-isClicked'){
-    body.className = 'toggle-isClicked';
-  divs.forEach(div => {
-    div.className = 'toggle-isClicked'
-  });
-  link.style.color = 'white';
-  } else {
-    body.className = '';
-  divs.forEach(div => {
-    div.className = ''
-  });
-  link.style.color = 'black';
-  }
+const bodyArticle = document.querySelector('#bodyArticle');
+const texts = document.querySelector('#textArticle p');
 
-})
-console.log(darkmodeBtn)
+if (body) {
+  darkmodeBtn.addEventListener('click', () => {
+    if(body.className !== 'toggle-isClicked'){
+      body.className = 'toggle-isClicked';
+    divs.forEach(div => {
+      div.className = 'toggle-isClicked'
+    });
+    link.style.color = 'white';
+    } else {
+      body.className = '';
+    divs.forEach(div => {
+      div.className = ''
+    });
+    link.style.color = 'black';
+    }
+  })
+} else {
+  darkmodeBtn.addEventListener('click', () => {
+    if(bodyArticle.className !== 'toggle-isClicked'){
+      bodyArticle.className = 'toggle-isClicked';
+      texts.style.color = 'white'
+    } else {
+      bodyArticle.className = '';
+      texts.style.color = 'black'
+    }
+  })
+}
