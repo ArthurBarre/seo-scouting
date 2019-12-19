08359a574c1a87f209f1973e288e1be788d3abfa
macarthur <arthurbarre.js@gmail.com>
2019-12-19 12:09:33 +0100

    add dm and robots

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,10 +1,14 @@
-const darkmodeBtn = document.querySelector('#darkmode');
+const darkmodeBtn = document.querySelector('#darkmode-container');
+const darkmodeMoon =  document.querySelector('.darkmode-image-moon');
+const darkmodeSun =  document.querySelector('.darkmode-image-sun');
 const divs = document.querySelectorAll('#thumbnail');
 const link =  document.querySelector('#link-thumbnail');
 const body =  document.querySelector('#body');
 const bodyArticle = document.querySelector('#bodyArticle');
 const texts = document.querySelector('#textArticle p');
 const logo = document.getElementById('logo');
+
+// darkmodeBtn.className = 'darkmode-image-moon';
 
 if (body) {
   darkmodeBtn.addEventListener('click', () => {
@@ -15,24 +19,31 @@ if (body) {
       div.className = 'toggle-isClicked'
     });
     link.style.color = 'white';
+    // darkmodeMoon.style.display = 'block'
+    // darkmodeSun.style.display = 'none'
+    darkmodeMoon.classList.remove = 'disable'
+    darkmodeSun.classList.add = 'disable'
     } else {
       body.className = '';
     divs.forEach(div => {
       div.className = ''
     });
-    link.style.color = 'black';
+    // link.style.color = 'black';
+    darkmodeMoon.classList.add = 'disable'
+    darkmodeSun.classList.remove = 'disable'
+    // darkmodeSun.style.display = 'block'
     }
   })
 } else {
   darkmodeBtn.addEventListener('click', () => {
-    if(bodyArticle.className !== 'toggle-isClicked'){
+    if (bodyArticle.className !== 'toggle-isClicked') {
       bodyArticle.className = 'toggle-isClicked';x
       texts.style.color = 'white'
-      
-
+      darkmodeBtn.className = 'darkmode-image-moon'
     } else {
       bodyArticle.className = '';
       texts.style.color = 'black'
+      darkmodeBtn.className = 'darkmode-image-sun'
     }
   })
 }
